ded735eff84bdefa915b31f5201dc6204f8febcc
Ian Bull <irbull@eclipsesource.com>
2023-07-20 14:22:17 -0700

    Generate redirects for blog posts
    Generate redirects for each blog post from the filename to its slug. This
    means that posts can be referenced by their filename, and the system
    will redirect to their canonical URL. This is useful for Obsidian
    or other local authoring as we can author in a WYSIWYG style.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -6,10 +5,64 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import { promises as fs } from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+function trimMdExtension(str) {
+  if (typeof str !== "string") {
+    return "Error: Input must be a string";
+  } else if (str.substr(-3) === ".md") {
+    return str.substring(0, str.length - 3);
+  } else {
+    return str;
+  }
+}
+
+function parseMarkdownFrontMatter(markdownString) {
+  try {
+    // Parse the markdown string
+    const result = matter(markdownString);
+
+    // Return the frontmatter data as a JavaScript object
+    return result.data;
+  } catch (error) {
+    // Handle/throw error
+    console.error("Error parsing markdown string: ", error);
+    throw new Error("Error parsing markdown string");
+  }
+}
+
+async function readMarkdownFiles(dir) {
+  let files = await fs.readdir(dir);
+  let redirects = {};
+
+  for (let file of files) {
+    let filePath = path.join(dir, file);
+    let stats = await fs.stat(filePath);
+
+    if (stats.isDirectory()) {
+      redirects = { ...redirects, ...(await readMarkdownFiles(filePath)) };
+    } else if (stats.isFile() && path.extname(filePath) === ".md") {
+      let frontMatter = parseMarkdownFrontMatter(
+        await fs.readFile(filePath, "utf-8")
+      );
+      if (frontMatter.postSlug) {
+        redirects[
+          trimMdExtension(filePath.replace("src/content", ""))
+        ] = `/posts/${frontMatter.postSlug}`;
+      }
+    }
+  }
+  return redirects;
+}
+
+const redirects = await readMarkdownFiles("./src/content/blog/");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  redirects,
   experimental: {
     assets: true,
   },
@@ -21,7 +74,6 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
-    image(),
   ],
   markdown: {
     remarkPlugins: [
